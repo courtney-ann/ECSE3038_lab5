8feb10c784484cad611ea24604e9254e6c8700a2
courtney-ann <95727276+courtney-ann@users.noreply.github.com>
2022-05-27 12:23:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,12 +38,30 @@ document.getElementById("newtank").addEventListener("click", function(event) {
 
 
 //GET request
-fetch ("http://localhost:5000/data")
-    .then((res) => res.json())
-    .then((json) => {
-        console.log(json)
-        var i;
-        for (i = 0; i < json.length ; i++) {
-            document.getElementsByClassName("tanks")[0].appendChild(createTankCard(json[i], i))
-        }
-    });
+var url = "http://localhost:3001/tank"
+        window.onload = function() {
+            var getOptions = {
+                method: "GET"
+            };
+            
+            fetch(url)
+            .then((res) => res.json())
+            .then((tanks) => {
+                console.log(tanks);
+                var body = document.querySelector("body");
+                
+                tanks.forEach((tank) => {
+                    console.log(tank._id["$oid"]);
+                    var tankCard = document.createElement("tank-card");
+                    tankCard.setAttribute("tank_id", tank._id["$oid"]);
+                    tankCard.setAttribute("latitude", tank.latitude);
+                    tankCard.setAttribute("longitude", tank.longitude);
+                    tankCard.setAttribute("percentage_full", tank.percentage_full);
+                    tankCard.setAttribute("location", tank.location);
+                    tankCard.setAttribute("image", "https://image.sciencenorway.no/1438480.jpg?imageId=1438480&width=1058&height=604");
+                    body.appendChild(tankCard);
+                });   // use data collected from database to create card
+
+            });
+            
+        };
